70b8f5ce512e5856e669830b570a0459b003947a
Sergey Kononenko <kononencheg@gmail.com>
2018-12-02 20:11:27 +0300

    Собирается с clickhouse; Обработка SELECT запроса.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,90 @@
+#include "Client/Connection.h"
+#include <IO/WriteBufferFromFileDescriptor.h>
+#include <Interpreters/Context.h>
 #include <iostream>
-#include <map>
+
+
+static std::shared_ptr<DB::IBlockOutputStream> block_out_stream;
+
+bool receiveResponse(
+        const DB::Context & context,
+        DB::WriteBufferFromFileDescriptor *std_out,
+        DB::Connection *connection)
+{
+    DB::Connection::Packet packet = connection->receivePacket();
+
+    switch (packet.type)
+    {
+        case DB::Protocol::Server::Data:
+
+            if (packet.block) {
+                std::cerr << packet.block.rows() << " Data Rows!" << std::endl;
+
+                if (!block_out_stream) {
+                    block_out_stream = context.getOutputFormat("JSONEachRow", *std_out, packet.block);
+                    block_out_stream->writePrefix();
+                }
+
+                if (packet.block.rows() != 0)
+                {
+                    block_out_stream->write(packet.block);
+                }
+
+                block_out_stream->flush();
+            } else {
+                std::cerr << "Empty Data!" << std::endl;
+            }
+
+            return true;
+
+        case DB::Protocol::Server::Progress:
+            std::cerr << "Progress!" << std::endl;
+            return true;
+
+        case DB::Protocol::Server::ProfileInfo:
+            std::cerr << "ProfileInfo!" << std::endl;
+            return true;
+
+        case DB::Protocol::Server::Totals:
+            std::cerr << "Totals!" << std::endl;
+            return true;
+
+        case DB::Protocol::Server::Extremes:
+            std::cerr << "Extremes!" << std::endl;
+            return true;
+
+        case DB::Protocol::Server::Exception:
+            std::cerr << "Exception!" << std::endl;
+            return true;
+
+        case DB::Protocol::Server::Log:
+            std::cerr << "Log!" << std::endl;
+            break;
+
+        case DB::Protocol::Server::EndOfStream:
+            std::cerr << "EndOfStream!" << std::endl;
+            return false;
+
+        default:
+            std::cerr << "StreamError!" << std::endl;
+    }
+
+    return false;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    DB::Context context = DB::Context::createGlobal();
+    DB::WriteBufferFromFileDescriptor std_out(STDOUT_FILENO);
 
-    std::map<char, uint> my_map;
-    uint d = 0;
+    DB::ConnectionTimeouts timeouts(
+            Poco::Timespan(5, 0),
+            Poco::Timespan(5, 0),
+            Poco::Timespan(5, 0));
 
-    while (d < 10) {
-        my_map.insert({ 'g', d++ });
-    }
+    DB::Connection connection("127.0.0.1", 9000, "", "default", "", timeouts);
+    connection.sendQuery("SELECT 1+1");
+
+    while (receiveResponse(context, &std_out, &connection)) {}
 
     return 0;
 }
